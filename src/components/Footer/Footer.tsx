4c9a0682b17cfd0f40ad7204d62bcd1edba066a6
annguyen <nguyentruongan16021996@gmail.com>
2022-10-24 19:17:42 +0700

    update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <div className="footer bg-dark p-3">
+    <div className="footer bg-dark p-4">
       <div className="row">
-        <div className="leftComponent col-4">
+        <div className="leftComponent col-10 col-md-5 col-lg-4 mb-md-5 mb-5">
           <div className="logo">
             <img src={cyber} alt="..." height={40} />
             <p className="text-white">
@@ -22,50 +22,50 @@ export default function Footer({}: Props) {
               dẫn cho các bạn
             </p>
             <div className="form d-flex">
-            <div className="form-floating mb-3 col-8">
-              <input
-                type="email"
-                className="form-control"
-                id="floatingPassword"
-                placeholder="Email liên hệ"
-              />
-              <label htmlFor="floatingPassword">Email</label>
-            </div>
-              <button className="btn btn-warning">ĐĂNG KÝ</button>
+              <div className="form-floating mb-3 col-8">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Email liên hệ"
+                />
+                <label htmlFor="floatingPassword">Email</label>
+              </div>
+              <button className="btn">ĐĂNG KÝ</button>
             </div>
           </div>
           <div className="location">
             <div className="locat1">
-              <i className="bi bi-geo-alt-fill text-white"></i>
+              <i className="bi bi-geo-alt-fill text-white me-2"></i>
               <span className="text-white">
                 Cơ sở 1: 376 Võ Văn Tần - Quận 3
               </span>
             </div>
             <div className="locat1">
-              <i className="bi bi-geo-alt-fill text-white"></i>
+              <i className="bi bi-geo-alt-fill text-white me-2"></i>
               <span className="text-white">
                 Cơ sở 2: 459 Sư Vạn Hạnh - Quận 10
               </span>
             </div>
             <div className="locat1">
-              <i className="bi bi-geo-alt-fill text-white"></i>
+              <i className="bi bi-geo-alt-fill text-white me-2"></i>
               <span className="text-white">
                 Cơ sở 3: 376 Võ Văn Tần - Bình Thạnh
               </span>
             </div>
             <div className="locat1">
-              <i className="bi bi-geo-alt-fill text-white"></i>
+              <i className="bi bi-geo-alt-fill text-white me-2"></i>
               <span className="text-white">
                 Cơ sở 4: Đà Nẵng - Quận Hải Châu
               </span>
             </div>
             <div className="phone">
-              <i className="bi bi-telephone-fill text-white"></i>
+              <i className="bi bi-telephone-fill text-white me-2"></i>
               <span className="text-white">096.105.1014 - 098.407.5835</span>
             </div>
           </div>
         </div>
-        <div className="centerComponent col-4">
+        <div className="centerComponent col-10 col-md-5 col-lg-4 mb-5">
           <h4 className="text-white">ĐĂNG KÝ TƯ VẤN</h4>
           <div>
             <div className="form-floating mb-3">
@@ -95,7 +95,7 @@ export default function Footer({}: Props) {
               />
               <label htmlFor="floatingPassword">Điện thoại liên hệ</label>
             </div>
-            <button className="btn btn-warning">ĐĂNG KÝ TƯ VẤN</button>
+            <button className="btn">ĐĂNG KÝ TƯ VẤN</button>
           </div>
           <div className="linkCourse">
             <a href="#">Lập trình Front End</a>
@@ -108,14 +108,26 @@ export default function Footer({}: Props) {
             <a href="#">Lập trình Java Spring - Java Boot</a>
             <a href="#">Phân tích Dữ liệu với Python</a>
             <a href="#">Tôi Đi Code Dạo</a>
-            
           </div>
-          
         </div>
-        <div className="rightComponent col-4">
-       <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flophocviet&tabs=timeline&width=340&height=331&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width={400} height={331} style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder={0}  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" />
-
-
+        <div className="rightComponent col-10 col-md-5 col-lg-4">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flophocviet&tabs=timeline&width=340&height=331&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            width={400}
+            height={331}
+            style={{ border: "none", overflow: "hidden" }}
+            scrolling="no"
+            frameBorder={0}
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          />
+          <div className="link">
+            <a href="#">Anh ngữ giao tiếp</a>
+            <a href="#"> Khởi động anh ngữ giao tiếp</a>
+            <a href="#"> Lấy đà anh ngữ giao tiếp</a>
+            <a href="#"> Bật nhảy anh ngữ giao tiếp</a>
+            <a href="#">Bay trên không Anh ngữ giao tiếp</a>
+            <a href="#">Tiếp đất</a>
+          </div>
         </div>
       </div>
     </div>
